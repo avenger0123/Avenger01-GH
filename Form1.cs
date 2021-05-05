046bbde9c577e1ec5e0dc90663be277a273ec6d7
jriedel <jriedel@A222-10>
2021-05-05 09:03:50 +0200

    JinyPozdrav

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace Avenger01
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Ahoj");
+            MessageBox.Show("Ahoj  xxxxxxxxxxxxc");
         }
     }
 }
